aba93b4344ea0ebf14eb659c31f9f5096812b0aa
Jack Fontinell <92953405+JackFontinell@users.noreply.github.com>
2022-03-17 14:14:52 -0700

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,35 @@
+from djitellopy import tello
+import numpy as np
+from time import sleep
+import math
+
+tello = tello.Tello()
+tello.connect()
+print(tello.get_battery())
+
+
+tello.takeoff()
+
+tello.move_up(90)
+sleep(2)
+
+tello.move_forward(500)
+sleep(0.1)
+
+tello.move_forward(500)
+sleep(0.1)
+
+#stop forward command
+tello.send_rc_control(0,0,0,0)
+sleep(2)
+
+#rotate while turning x and y direction
+tello.send_rc_control(0, 30, 0, 40)
+sleep(10)
+
+#stop turn and rotation
+tello.send_rc_control(0, 0, 0, 0)
+sleep(2)
+
+tello.land()
+#610 cm sideways
